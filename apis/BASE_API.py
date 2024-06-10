cbd14e9a06666065b0456ceb7a3812c6604bd9c4
raghu2prasad <31303256+raghu2prasad@users.noreply.github.com>
2024-06-10 13:57:49 +0530

    basic changes done for headers

--- apis/BASE_API.py
+++ apis/BASE_API.py
@@ -2,10 +2,21 @@ import requests
 import json
 
 url = "http://hapi.fhir.org/baseR4/Observation/2529509/_history/1?_pretty=true"
-headers = {"Server": "Jetty(12.0.8)",
- "X-Powered-By":"HAPI FHIR 7.3.1-SNAPSHOT/d413af1777/2024-05-11 REST Server (FHIR Server; FHIR 4.0.1/R4)",
-           "Content-Type": "application/fhir+xml;charset=utf-8"}
-response = requests.get(url = url)
+headers = {
+    "Accept-Charset": "utf-8",
+    "Accept":"application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9, application/json+fhir;q=0.9",
+           "User-Agent": " HAPI-FHIR/7.3.1-SNAPSHOT (FHIR Client; FHIR 4.0.1/R4; apache)"}
+#           "Accept-Encoding" : "gzip"}
+headers1 = {
+    "Accept-Charset": "utf-8",
+    "Accept": "application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9, application/json+fhir;q=0.9",
+    "User-Agent": "HAPI-FHIR/7.3.1-SNAPSHOT (FHIR Client; FHIR 4.0.1/R4; apache)",
+    "Accept-Encoding": "gzip"
+}
+
+response = requests.get(url = url,headers=headers1)
 print(response.status_code)
 assert response.status_code == 200
-print(response.json())
+#print(response.json())
+print(response.text)
+print(type(response.text))
